67237284ff5fc0749f05a2a195efc0b59a4bad3e
Venomity <veeti.hietasalo@hotmail.fi>
2024-12-21 22:28:52 +0200

    last small changes to main.ts + removing SSR

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,12 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [],
-  imports: [BrowserModule, AppComponent],
-  providers: [],
-  bootstrap: [] // Bootstrapping AppComponent
+  imports: [BrowserModule, AppComponent],  // Import AppComponent here
 })
 export class AppModule {}
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,15 +1,10 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from '@/app.component';
+import { provideRouter } from '@angular/router';
+import { routes } from '@/app.routes';
 
-import { AppModule } from '@/app.module';
-import { environment } from './environments/environment';
-
-// Enable production mode if required
-if (environment.production) {
-  enableProdMode();
-}
-
-// Bootstrap the main application module
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch(err => console.error('Error bootstrapping the application:', err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes)
+  ]
+}).catch(err => console.error(err));
